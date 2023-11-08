3acfdf6a83e89618b1507a7ba5a5a8664ae0777f
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-11-07 19:36:25 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS ?? '', {
     host: process.env.BD_HOST,
-    port: 3306,
+    port: 3307,
     dialect: 'mysql',
     define: {
         timestamps: true
